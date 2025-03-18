7c9d43b401f9d543735d9ee08e2eb3d320c04920
peter <peterkinyanjui77@yahoo.com>
2025-03-18 13:20:07 +0300

    Addition Calculator,Intro to Python assignment 1

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#Addition calculater
+def num_add():
+    add = num1 + num2
+    return add
+
+num1 =int(input ('Enter First Number: '))
+num2 = int(input ('Enter Second Number: '))
+
+print('The sum of the two number is:',num_add())
